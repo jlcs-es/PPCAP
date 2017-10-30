2353f297910fb937a4c3e7dd6fc276fe033b6fb9
José Luis Cánovas Sánchez <joseluis.canovas@outlook.com>
2017-10-30 15:51:02 +0100

    avance mpp

--- MPP/MPI+OpenMP/BC/sec.cpp
+++ MPP/MPI+OpenMP/BC/sec.cpp
@@ -2,8 +2,7 @@
 CPP_CONTEST=2017
 CPP_PROBLEM=B
 CPP_LANG=C+MPI
-CPP_PROCESSES_PER_NODE=1
-CPP_NUM_NODES=1
+CPP_PROCESSES_PER_NODE=calisto 4
 */
 
 #include <stdlib.h>
@@ -33,12 +32,20 @@ void trasponer(int n,double *a) {
 
 
 void sec(int n,double *a,double *b,double *c,int nodo,int np) {
-    
-    int *zerosrows=new int[n],*zeroscolumns=new int[n];
+    MPI_Bcast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
+    if (nodo == 0) {
+        int *zerosrows=new int[n],*zeroscolumns=new int[n];
 
-    trasponer(n,b);
-    obtainzerosrows(n,a,zerosrows);
-    obtainzerosrows(n,b,zeroscolumns);
+        trasponer(n,b);
+        obtainzerosrows(n,a,zerosrows);
+        obtainzerosrows(n,b,zeroscolumns);
+    }
+    if(nodo!=0)
+    {
+        a=(double *) malloc(sizeof(double)*n*n/np);
+        b=(double *) malloc(sizeof(double)*n*n);
+        c=(double *) malloc(sizeof(double)*n*n/np);
+    }
 
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++) {
